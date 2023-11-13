6bd46ffddaa39d2a005a11f201c6331f790b28f2
Shokkobarokko <avetis.01@mail.ru>
2023-11-14 00:52:31 +0300

    Initial Commit

---
+++ Sprint_3_Home_Work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sprint_3_Home_Work
+//
+//  Created by Аветис Парсаданян on 11/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
